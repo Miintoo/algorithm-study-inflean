c0426fa8b5256f481766e5a2cddd63ed45b16b0e
김민혁 <minto@gimminhyeog-ui-MacBookPro.local>
2024-12-02 20:52:20 +0900

    solved - two-sum with two pointer - minto

--- minto/List/반복문/two-sum.js
+++ minto/List/반복문/two-sum.js
@@ -14,3 +14,26 @@ var twoSum = function(nums, target) {
     }
     return false;
 };
+
+// 투 포인터로 해결 결과는 leetcode와 two-sum과 다르다.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let [l, r] = [0, nums.length - 1];
+    nums.sort((a,b) => {return a>b ? 1 : a<b ? -1 : 0;});
+    while(l < r) {
+        if (nums[l] + nums[r] > target) {
+            r -= 1;
+        } else if (nums[l] + nums[r] < target) {
+            l += 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
+
+};
